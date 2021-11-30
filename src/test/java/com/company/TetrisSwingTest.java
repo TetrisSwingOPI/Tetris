1fc4164f6251c621190af70001e7e9fe1b80ddd9
freezton <81812309+freezton@users.noreply.github.com>
2021-11-30 23:07:49 +0300

    Add files via upload

--- src/test/java/com/company/TetrisSwingTest.java
+++ src/test/java/com/company/TetrisSwingTest.java
@@ -1,13 +1,18 @@
 package com.company;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
 
 class TetrisSwingTest {
-
-    @org.junit.jupiter.api.Test
-    void showRestartLabel() {
-        int actual = 5+5;
-        int expected = 10;
-        assertEquals(actual, expected);
+    @Test
+    @DisplayName("RestartGameTest")
+    void restartGameTest() {
+        boolean expectedRestart = false;
+        TetrisSwing.restartGame();
+        boolean actualRestart = TetrisSwing.restart;
+        Assertions.assertEquals(expectedRestart, actualRestart);
     }
 }
